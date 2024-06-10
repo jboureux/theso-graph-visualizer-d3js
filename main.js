2cca0b493a9765b9d527619d961085ac760c8893
Jérémy Boureux <jeremyboureux@gmail.com>
2024-06-10 16:15:22 +0200

    ajout export svg

--- main.js
+++ main.js
@@ -33,3 +33,17 @@ document
             labels.forEach((label) => label.classList.add("hide-label"));
         }
     });
+
+document.querySelector("#download-svg").addEventListener("click", () => {
+    const svg = document.querySelector("#graph").innerHTML;
+    download(svg, "image/svg+xml", "test.svg");
+});
+
+function download(content, mimeType, filename) {
+    const a = document.createElement("a"); // Create "a" element
+    const blob = new Blob([content], { type: mimeType }); // Create a blob (file-like object)
+    const url = URL.createObjectURL(blob); // Create an object URL from blob
+    a.setAttribute("href", url); // Set "a" element link
+    a.setAttribute("download", filename); // Set download filename
+    a.click(); // Start downloading
+}
